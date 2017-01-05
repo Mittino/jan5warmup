312880c8e5c962c722f37e9826fbe0de645f0f0b
mittino <annamefford@gmail.com>
2017-01-05 11:51:30 -0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const code = require('../jan5warmup.js');
+const expect = require('chai').expect;
+
+describe("targetSumIndex", () => {
+
+  it("Should accept an array and an integer", () => {
+    //expect(code.targetSumIndex()).to.equal("invalid argument length");
+    expect(code.targetSumIndex('string', 'string')).to.equal("invalid argument type");
+    //expect(code.targetSumIndex({}, [])).to.equal("invalid argument type");
+  });
+
+  // it("Should return an array with two index numbers that sum to equal the second argument", () => {
+  //   expect(code.targetSumIndex([1,2,3,4], 4)).to.equal([0,2]);
+  //   expect(code.targetSumIndex([1,2,5], 7)).to.equal([1,2]);
+  // });
+
+});
